161a547d01dd9927316674a63e834dee4f8a2f73
kylehuang041 <kylehuang041@gmail.com>
2024-05-04 19:17:38 -0700

    changed view zoomg

--- Assets/Scripts/Car/CarDestroy.cs
+++ Assets/Scripts/Car/CarDestroy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CarDestroy : MonoBehaviour
 {
-    public float boundary = 15.0f;
+    public float boundary = 20.0f;
 
     // Update is called once per frame
     void Update()
